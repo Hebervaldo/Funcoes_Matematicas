5522bc2528c53258b927b259c6908b4f3183b133
Hebervaldo <hebervaldo@hotmail.com>
2022-03-05 08:02:09 -0300

    Funcoes_Matematicas: Enviando arquivos atualizados *.c

--- Rotina_git_Enviar_Alteracoes.sh
+++ Rotina_git_Enviar_Alteracoes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usuario="Hebervaldo"
-token="$(cat '"'/home/usuario/Documentos/git/$usuario/Token_Git_Hebervaldo.txt'"')"
+token="$(cat '/home/usuario/Documentos/git/$usuario/Token_Git_Hebervaldo.txt')"
 nome_repositorio="Funcoes_Matematicas"
 
 for n in *.c
