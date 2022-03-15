b0037df0cd06f5a529da24cee83551c5cf3516a3
Hebervaldo <hebervaldo@hotmail.com>
2022-03-14 21:57:03 -0300

    Funcoes_Matematicas: Enviando arquivos atualizados *.c

--- div.c
+++ div.c
@@ -1,35 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
+#define CONSTANTE_TIPO_NUMERO_DIGITOS "%.18Lf\n"
+
+long double mtdOperarDivisao(long double x, long double y)
+{
+	return x / y;
+}
+
 int main(int argc, char *argv[])
 {
-    long double x = atof(argv[1]);
-    long double y = atof(argv[2]);
-    long double Resultado = x / y;
+    long double x = 0;
+    long double y = 0;
+    long double Resultado = 0;
 
 	FILE *cfPtr;
 
     switch (argc)
 	{
 	case 1:
-        FILE *cfPtr;
         cfPtr = fopen("div.input", "r");
-        fscanf(cfPtr, "%d", &numEscondida);
+        fscanf(cfPtr, "%Lf", &x);
+        fscanf(cfPtr, "%Lf", &y);
 
-        fclose(cfPtr);
-    break;
-	case 3:
-        double x = atof(argv[1]);
-        double y = atof(argv[2]);
+		Resultado = mtdOperarDivisao(x, y);
 
-		printf("%.100Lf\n", Resultado);
+		printf(CONSTANTE_TIPO_NUMERO_DIGITOS, Resultado);
 
 		cfPtr = fopen("div.result", "w");
-		fprintf(cfPtr, "%.100Lf\n", Resultado);
-		fclose(cfPtr);
-		break;
+		fprintf(cfPtr, CONSTANTE_TIPO_NUMERO_DIGITOS, Resultado);
+    break;
+	case 3:
+		cfPtr = fopen("div.result", "w");
+        x = atof(argv[1]);
+        y = atof(argv[2]);
+
+		Resultado = mtdOperarDivisao(x, y);
+
+		printf(CONSTANTE_TIPO_NUMERO_DIGITOS, Resultado);
+
+		cfPtr = fopen("div.result", "w");
+		fprintf(cfPtr, CONSTANTE_TIPO_NUMERO_DIGITOS, Resultado);
+	break;
 	default:
-		printf("exp: numero invalido de argumentos.\n");
-		break;
+		printf("div: numero invalido de argumentos.\n");
+	break;
 	}
+
+    fclose(cfPtr);
 }
--- mul.c
+++ mul.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
+#define CONSTANTE_TIPO_NUMERO_DIGITOS "%.18Lf\n"
+
+long double mtdOperarMultiplicacao(long double x, long double y)
+{
+	return x * y;
+}
+
 int main(int argc, char *argv[])
 {
-    long double x = atof(argv[1]);
-    long double y = atof(argv[2]);
-    long double Resultado = x * y;
+    long double x = 0;
+    long double y = 0;
+    long double Resultado = 0;
 
 	FILE *cfPtr;
 
     switch (argc)
 	{
-	case 3:
-		printf("%.100Lf\n", Resultado);
+	case 1:
+        cfPtr = fopen("mul.input", "r");
+        fscanf(cfPtr, "%Lf", &x);
+        fscanf(cfPtr, "%Lf", &y);
+
+		Resultado = mtdOperarMultiplicacao(x, y);
+
+		printf(CONSTANTE_TIPO_NUMERO_DIGITOS, Resultado);
 
 		cfPtr = fopen("mul.result", "w");
-		fprintf(cfPtr, "%.100Lf\n", Resultado);
-		fclose(cfPtr);
-		break;
+		fprintf(cfPtr, CONSTANTE_TIPO_NUMERO_DIGITOS, Resultado);
+    break;
+	case 3:
+		cfPtr = fopen("mul.result", "w");
+        x = atof(argv[1]);
+        y = atof(argv[2]);
+
+		Resultado = mtdOperarMultiplicacao(x, y);
+
+		printf(CONSTANTE_TIPO_NUMERO_DIGITOS, Resultado);
+
+		cfPtr = fopen("mul.result", "w");
+		fprintf(cfPtr, CONSTANTE_TIPO_NUMERO_DIGITOS, Resultado);
+	break;
 	default:
-		printf("exp: numero invalido de argumentos.\n");
-		break;
+		printf("mul: numero invalido de argumentos.\n");
+	break;
 	}
+
+    fclose(cfPtr);
 }
